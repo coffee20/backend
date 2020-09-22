d6f803b76b43bb62b87aee0c4480d123c7bf65e4
s4ng <zalcls951201@gmail.com>
2020-09-22 13:14:47 +0900

    0922 upload

--- routes/coffeeRouter.js
+++ routes/coffeeRouter.js
@@ -2,74 +2,78 @@ const router = require('express').Router();
 const Coffee = require('../models/coffee');
 
 // CREATE COFFEE
-router.post('/coffeeInsert', function(req, res){
-    let coffee = new Coffee();
-    coffee.coffeeName = req.body.coffeeName;
-    coffee.cafeName = req.body.cafeName;
+router.post('/coffeeInsert', function (req, res) {
+	let coffee = new Coffee();
+	coffee.coffeeName = req.body.coffeeName;
+	coffee.cafeName = req.body.cafeName;
 
-    coffee.save(function(err){
-        if(err){
-            console.error(err);
-            res.json({result: 0});
-            return;
-        }
+	coffee.save(function (err) {
+		if (err) {
+			console.error(err);
+			res.json({ result: 0 });
+			return;
+		}
 
-        res.json({result: 1, message: "Save success!"});
-    });
+		res.json({ result: 1, message: "Save success!" });
+	});
 });
 
 // Get Coffee ID by Cafe name and coffee name
 // 카페이름, 커피이름으로 커피 id 찾기
 router.get('/getCoffeeId/:Cafe_name/:Coffee_name', (req, res) => {
-	Coffee.findOne({coffeeName: req.params.Coffee_name, cafeName: req.params.Cafe_name}, (err, coffee) => {
-        if (err) return res.status(500).json({error: err});
-        if (!coffee) return res.status(404).json({error: "Coffee is not exist"});
-		res.status(200).json({_id: coffee._id});
+	Coffee.findOne({ coffeeName: req.params.Coffee_name, cafeName: req.params.Cafe_name }, (err, coffee) => {
+		if (err) return res.status(500).json({ error: err });
+		if (!coffee) return res.status(404).json({ error: "Coffee is not exist" });
+		res.status(200).json({ _id: coffee._id });
 	})
 })
 
 // Get coffee's star point average
 router.get('/getCoffeePointAvg/:coffee_id', (req, res) => {
 	Coffee.findById(req.params.coffee_id, (err, coffee) => {
-        if (err) throw err;
-		if (!coffee) return res.status(404).json({error: "Coffee is not exist"});
+		if (err) throw err;
+		if (!coffee) return res.status(404).json({ error: "Coffee is not exist" });
 
 		let coffeePoints = coffee.toObject().points;
 		let avg, sum = 0;
-		for(let i = 0; i < coffeePoints.length; i++){
-			sum += coffeePoints[0].point;
+		for (let i = 0; i < coffeePoints.length; i++) {
+			sum += coffeePoints[i].point;
 		}
 		avg = (sum / coffeePoints.length).toFixed(1);
-		res.status(200).json({"avg": avg});
+		res.status(200).json({ "avg": avg });
 	})
 })
 
 // Coffee Star point insert
 router.put('/coffeeStarInsert/:coffee_id/:user_id/:point', (req, res) => {
 	Coffee.findByIdAndUpdate(req.params.coffee_id,
-		{ "$push" : {"points": {
-			"userId" : req.params.user_id,
-			"point" : req.params.point
-		}}},
-		{"new" : true, "upsert": true},
+		{
+			"$push": {
+				"points": {
+					"userId": req.params.user_id,
+					"point": req.params.point
+				}
+			}
+		},
+		{ "new": true, "upsert": true },
 		(err, coffee) => {
-			if(err) throw err;
-        	if (!coffee) return res.status(404).json({error: "Coffee is not exist"});
-			res.json({result: 1, message: "star point success! : coffee"});
-		} 
+			if (err) throw err;
+			if (!coffee) return res.status(404).json({ error: "Coffee is not exist" });
+			res.json({ result: 1, message: "star point success! : coffee" });
+		}
 	);
-} )
+})
 
 // Coffee Star Point delete
 router.put('/coffeeStarDelete/:coffee_id/:user_id', (req, res) => {
 	Coffee.findByIdAndUpdate(req.params.coffee_id,
-		{ "$pull" : {"points" : {"userId" : req.params.user_id} } },
+		{ "$pull": { "points": { "userId": req.params.user_id } } },
 		(err, star) => {
-			if(err) throw err;
-        	if (!star) return res.status(404).json({error: "star is not exist"});
-			res.json({result: 1, message: "star point deleted! : coffee"})
-		} 
+			if (err) throw err;
+			if (!star) return res.status(404).json({ error: "star is not exist" });
+			res.json({ result: 1, message: "star point deleted! : coffee" })
+		}
 	)
-} )
+})
 
 module.exports = router;
