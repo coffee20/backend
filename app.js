42485b751b191895d992f535cd42b1d8564e9daf
s4ng <zalcls951201@gmail.com>
2020-10-22 21:56:02 +0900

    add privacy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const path = require('path');
 const db = require('./src/db');
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.use(logger('dev'));
 
 // routing
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/privacy', (req, res) => {
+    res.sendFile(path.join(__dirname+'/src/privacy.html'));
+})
 app.use('/api/user', require('./routes/userRouter'));
 app.use('/api/cafe', require('./routes/cafeRouter'));
 app.use('/api/coffee', require('./routes/coffeeRouter'));
